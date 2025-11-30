0506848f39a61de4e4791a6bccd7ca329f278076
monkeymechanicsgames <monkeymechanicsgames@gmail.com>
2025-11-30 08:17:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const memeImages = [
   {
      preview: "warriorcats.png",  // This is the grid image you click
     memes: ["warriorcats-1.png", "warriorcats-2.png", "warriorcats-3.png", "warriorcats-4.png"] // These 4 images show in th
-     preview: "roblox.png", 
+     
   }
+  {
+     preview: "roblox.png",
+
+
 ];
 
 const grid = document.getElementById("imageGrid");
